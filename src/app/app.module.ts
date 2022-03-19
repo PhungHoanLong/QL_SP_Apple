d2b388a1c9619cfe89d89c2e12a9d0b3ccb41cd8
PhungHoanLong <long1594673@gmail.com>
2022-03-19 17:10:19 +0700

    Change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { ProductComponent } from './product/product.component';
+import { StoreComponent } from './store/store.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    ProductComponent,
+    StoreComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
